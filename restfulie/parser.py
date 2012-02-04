7981c85e1bc3490bcbc6f98fc9ccf8a23e5191f8
Carlos Martín <cmartin@liberalia.net>
2012-02-04 19:17:20 +0100

    refactor parser module

--- restfulie/parser.py
+++ restfulie/parser.py
@@ -1,11 +1,28 @@
+#!/usr/bin/env python
+# -*- mode:python; tab-width: 2; coding: utf-8 -*-
+
+"""
+parser
+"""
+
+from __future__ import absolute_import
+
+__author__  = "Carlos Martin <cmartin@liberalia.net>"
+__license__ = "See LICENSE file for details"
+
+# Import here any required modules.
+
+__all__ = ['Parser']
+
+
+#pylint: disable-msg=R0903
 class Parser(object):
-    """
-    Executes processors ordered by the list
-    """
+    """Executes processors ordered by the list"""
 
     def __init__(self, processors):
-        self.processors = processors
+        self._processors = processors
 
-    def follow(self, callback, request, env={}):
-        processor = self.processors.pop(0)
-        return processor.execute(callback, self, request, env)
+    def follow(self, callback, request, env=None):
+        """Follow chain"""
+        processor = self._processors.pop(0)
+        return processor.execute(callback, self, request, env or {})
