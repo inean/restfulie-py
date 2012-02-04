5abc2444346aecfba4b6b9aeaade54a587ec4cf3
Carlos Martín <cmartin@liberalia.net>
2012-02-04 20:00:16 +0100

    refactor processor to work properly with Tornado

--- restfulie/processor.py
+++ restfulie/processor.py
@@ -1,31 +1,73 @@
+#!/usr/bin/env python
+# -*- mode:python; tab-width: 2; coding: utf-8 -*-
+
+"""
+processors
+"""
+
+from __future__ import absolute_import
+
+__author__  = "Carlos Martin <cmartin@liberalia.net>"
+__license__ = "See LICENSE file for details"
+
+# Import here any required modules.
+
+__all__ = ['Configuration']
+
+# Project requirements
 from base64 import encodestring
-from response import Response
-from converters import Converters
+from copy import copy
 from tornado.httpclient import HTTPClient, AsyncHTTPClient
-import restfulie
 
+# local submodule requirements
+from .converters import Converters
+from .response import Response
+from .request import Request
 
+__all__ = [
+    'AuthenticationProcessor',
+    'ExecuteRequestProcessor',
+    'PayloadMarshallingProcessor',
+    'RedirectProcessor',
+    ]
+
+#pylint: disable-msg=R0903
 class RequestProcessor(object):
-    def execute(self, callback, chain, request, env={}):
+    """Base class for all processors"""
+
+    def execute(self, callback, chain, request, env):
+        """Command called to be runned"""
         raise NotImplementedError('Subclasses must implement this method')
 
 
+#pylint: disable-msg=R0903, R0922
 class AuthenticationProcessor(RequestProcessor):
-    """
-    Processor responsible for making HTTP simple auth
-    """
-    def execute(self, callback, chain, request, env={}):
-        if request.credentials is not None:
-            encoded_credentials = self._encode_credentials(request.credentials)
-            request.headers['authorization'] = "Basic %s" % encoded_credentials
+    """Abstract class for authentication methods"""
+
+    USERNAME, PASSWORD, METHOD = xrange(0, 3)
+
+    implements = None
+
+    def execute(self, callback, chain, request, env):
+        credentials = request.credentials
+        if credentials and credentials[self.METHOD] == self.implements:
+            request.headers['authorization'] = self._encode(request.credentials)
         return chain.follow(callback, request, env)
 
-    def _encode_credentials(self, credentials):
-        username = credentials[0]
-        password = credentials[1]
-        method = credentials[2]
-        if (method == 'simple'):
-            return encodestring("%s:%s" % (username, password))[:-1]
+    def _encode(self, credentials):
+        """Return authentication header value"""
+        raise NotImplementedError
+
+
+#pylint: disable-msg=R0903
+class AuthenticationSimpleProcessor(AuthenticationProcessor):
+    """Processor responsible for making HTTP simple auth"""
+
+    implements = "simple"
+
+    def _encode(self, credentials):
+        username, password, _ = credentials
+        return "Basic %s" % encodestring("%s:%s" % (username, password))[:-1]
 
 
 class ExecuteRequestProcessor(RequestProcessor):
@@ -33,7 +75,9 @@ class ExecuteRequestProcessor(RequestProcessor):
     Processor responsible for getting the body from environment and
     making a request with it.
     """
-    def _sync(self, chain, request, env={}):
+    @staticmethod
+    def _sync(request, env):
+        """Run blocked"""
         response = HTTPClient().fetch(
             request.uri,
             method=request.verb,
@@ -41,7 +85,9 @@ class ExecuteRequestProcessor(RequestProcessor):
             headers=request.headers)
         return Response(response)
 
-    def _async(self, callback, chain, request, env={}):
+    @staticmethod
+    def _async(callback, request, env):
+        """Run async"""
         AsyncHTTPClient().fetch(
             request.uri,
             lambda x: callback(Response(x)),
@@ -50,47 +96,61 @@ class ExecuteRequestProcessor(RequestProcessor):
             headers=request.headers)
         return None
 
-    def execute(self, callback, chain, request, env={}):
-        return self._sync(chain, request, env) \
+    def execute(self, callback, chain, request, env):
+        return self._sync(request, env) \
             if not callable(callback)          \
-            else self._async(callback, chain, request, env)
+            else self._async(callback, request, env)
 
 
 class PayloadMarshallingProcessor(RequestProcessor):
-    """
-    Processor responsible for marshalling the payload in environment.
-    """
-    def execute(self, callback, chain, request, env={}):
-        if "payload" in env:
-            content_type = request.headers.get("Content-Type")
-            marshaller = Converters.marshaller_for(content_type)
-            env["body"] = marshaller.marshal(env["payload"])
-            del(env["payload"])
+    """Responsible for marshalling the payload in environment"""
 
+    def execute(self, callback, chain, request, env):
+        if "payload" in env:
+            content_type = request.headers.get("content-type")
+            marshaller   = Converters.marshaller_for(content_type)
+            env["body"]  = marshaller.marshal(env["payload"])
+            del(env["payload"])
         return chain.follow(callback, request, env)
 
 
 class RedirectProcessor(RequestProcessor):
     """
-    A processor responsible for redirecting a client to another URI when the
-    server returns the location header and a response code related to
-    redirecting.
+    A processor responsible for redirecting a client to another URI
+    when the server returns the location header and a response code
+    related to redirecting.
     """
     REDIRECT_CODES = ['201', '301', '302']
 
-    def redirect_location_for(self, result):
-        if (result.code in self.REDIRECT_CODES):
-            return (result.headers.get("Location") or
-                    result.headers.get("location"))
-        return None
+    def _redirect(self, result):
+        """Get redirection url, if any"""
+        return result.headers.get("location") \
+            if (result.code in self.REDIRECT_CODES) else None
 
-    def execute(self, callback, chain, request, env={}):
+    def execute(self, callback, chain, request, env):
         def _on_resource(resource):
+            """resource callback"""
             assert callable(callback)
-            location = self.redirect_location_for(resource)
-            return callback(resource) if not location else \
-                restfulie.Restfulie.at(location).as_(request_type).get(callback)
+            # copy configuration and update url
+            url = self._redirect(resource)
+            if not url:
+                return callback(resource)
+            # make a new request
+            config = copy(request.config)
+            config.url = url
+            return Request(self)(callback)
         # chain
         return chain.follow(_on_resource, request, env)    \
             if callable(callback) else chain.follow(callback, request, env)
 
+
+# Main tornado chain (Use follow_redirects so RedirectProcessor is not
+# required anymore
+
+#pylint:disable-msg=C0103
+tornado_chain = [
+    AuthenticationSimpleProcessor(),
+#    RedirectProcessor(),
+    PayloadMarshallingProcessor(),
+    ExecuteRequestProcessor(),
+    ]
