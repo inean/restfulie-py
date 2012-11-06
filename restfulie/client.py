ddf2aa0fcc20fa732292536d702336f6729cedff
Carlos Martín <cmartin@liberalia.net>
2012-11-06 22:20:54 +0100

    Allow Client subclasses

--- restfulie/client.py
+++ restfulie/client.py
@@ -14,13 +14,48 @@ __license__ = "See LICENSE.restfulie for details"
 from contextlib import contextmanager
 from functools import wraps
 
-__all__ = ['Client']
+__all__ = ['Client', 'Extend']
 
 # local submodule requirements
 from .credentials import Credentials
 
+
+class Extend(type):
+    """
+    Allow Ad-hoc inheritance. See
+    http://code.activestate.com/recipes/412717-extending-classes/ for
+    details
+    """
+
+    clients = {}
+
+    #pylint: disable-msg=W0613
+    def __new__(mcs, name, bases, dct):
+        prev = Extend.clients[name]
+        del dct['__module__']
+        del dct['__metaclass__']
+        for key, value in dct.iteritems():
+            if key == "__doc__":
+                continue
+            setattr(prev, key, value)
+        return prev
+
+        
+class ClientMeta(type):
+    """
+    Simple metaclass to make client subclasses discoverable by
+    Extend metaclass
+    """
+    def __init__(mcs, name, bases, dct):
+        super(ClientMeta, mcs).__init__(name, bases, dct)
+        assert name not in Extend.clients
+        Extend.clients[name] = mcs
+
+            
 class Client(object):
     """Base class to implement a remote API"""
+
+    __metaclass__ = ClientMeta
 
     def __init__(self, credentials=None):
         self._config = credentials or Credentials()
@@ -34,31 +69,6 @@ class Client(object):
     def credentials(self):
         """Get credentials element"""
         return self._config
-
-    @property
-    def write_enabled(self):
-        """Check if config has valid credentials. Return false
-        otherwise"""
-        raise NotImplementedError
-
-
-class Extend(type):
-    """
-    Allow Ad-hoc inheritance. See
-    http://code.activestate.com/recipes/412717-extending-classes/ for
-    details
-    """
-
-    #pylint: disable-msg=W0613
-    def __new__(mcs, name, bases, dct):
-        prev = globals()[name]
-        del dct['__module__']
-        del dct['__metaclass__']
-        for key, value in dct.iteritems():
-            if key == "__doc__":
-                continue
-            setattr(prev, key, value)
-        return prev
 
 
 def validate(table):
